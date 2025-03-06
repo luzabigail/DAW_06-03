eb546407e68a88fd15c1fa41536720a7e62cba72
Luz <Luz@gmail.com>
2025-03-06 09:48:28 +0100

    Commit inicial

---
+++ src/Tienda/GestorTiendaTest.java
@@ -0,0 +1,34 @@
+package Tienda;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GestorTiendaTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testCalcularDescuento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCategorizarProducto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBuscarProducto() {
+		fail("Not yet implemented");
+	}
+
+}
